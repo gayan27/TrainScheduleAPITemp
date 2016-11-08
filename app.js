667bc4e81f150410066a33ed60d5f620950b7d2a
gayan27 <gssampath.27@gmail.com>
2016-11-08 21:49:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var services = require('./services/services.js');
+var config = require('./config.json'); 
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//To access TrainSchedule.json  file directly 
+app.use("/DataFiles", express.static(__dirname + '/res/TrainSchedule.json')); 
+services.serviceCall(app);
+
+//starting the server
+app.listen(config.SERVER_PORT, function (res, err) {
+    if (err) {
+        console.log('Somthing going wrong Please Check The Error :' + err);
+        res.status(500);
+    }
+    console.log('TrainSchedule is listening on port ' + config.SERVER_PORT);
+})
